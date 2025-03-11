841d51d274480461ac1c566754c6325c03c2263b
dkoeni <116151702+dkoeni@users.noreply.github.com>
2025-03-11 11:27:19 +0100

    sort alphabetically in autoformat_spec (#32)

--- .github/autoformat/autoformat_spec.js
+++ .github/autoformat/autoformat_spec.js
@@ -1,4 +1,4 @@
-// usage: node reorder.js <path_to_file>
+// usage: node autoformat_spec.js <path_to_file>
 
 const fs = require("fs");
 const { parseYaml, stringifyYaml } = require("@redocly/openapi-core");
@@ -23,18 +23,29 @@ const reorder_root = (root) => {
 
 const reorder_components = (components) => {
     const { schemas, responses, parameters, examples, requestBodies, headers, securitySchemes, links, callbacks, pathItems, ...rest } = components;
+
+    const sortObjectKeys = (obj) => {
+        if (!obj) return obj;
+        return Object.keys(obj)
+            .sort()
+            .reduce((sorted, key) => {
+                sorted[key] = obj[key];
+                return sorted;
+            }, {});
+    };
+
     return {
         // define components object order
-        schemas,
-        responses,
-        parameters,
-        examples,
-        requestBodies,
-        headers,
-        securitySchemes,
-        links,
-        callbacks,
-        pathItems,
+        schemas: sortObjectKeys(schemas),
+        responses: sortObjectKeys(responses),
+        parameters: sortObjectKeys(parameters),
+        examples: sortObjectKeys(examples),
+        requestBodies: sortObjectKeys(requestBodies),
+        headers: sortObjectKeys(headers),
+        securitySchemes: sortObjectKeys(securitySchemes),
+        links: sortObjectKeys(links),
+        callbacks: sortObjectKeys(callbacks),
+        pathItems: sortObjectKeys(pathItems),
         ...rest,
     };
 };
@@ -48,5 +59,4 @@ const content = fs.readFileSync(fileName, "utf8");
 let reordered = parseYaml(content);
 reordered = reorder_root(reordered);
 reordered.components = reorder_components(reordered.components);
-console.log(reordered)
 fs.writeFileSync(fileName, stringifyYaml(reordered, {'lineWidth': 150}));

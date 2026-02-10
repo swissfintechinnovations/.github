e50ff23921f67df572941efbbd1d3a1f1daf29a5
dkoeni <116151702+dkoeni@users.noreply.github.com>
2026-02-10 12:27:54 +0100

    Refactor imports and export syntax in redocly_sfti_rules.js

--- .github/redocly/redocly_sfti_rules.js
+++ .github/redocly/redocly_sfti_rules.js
@@ -1,53 +1,55 @@
-const checkSchemaName = require('./rules/data-types');
+import checkSchemaName from './rules/data-types.js';
 
-const checkObjectOrder = require('./rules/root-object-order');
-const checkOpenapiVersion = require('./rules/openapi-version');
-const infoStructure = require('./rules/info-content');
-const checkExternalDocs = require('./rules/external-docs');
-const componentsOrder = require('./rules/component-order');
+import checkObjectOrder from './rules/root-object-order.js';
+import checkOpenapiVersion from './rules/openapi-version.js';
+import infoStructure from './rules/info-content.js';
+import checkExternalDocs from './rules/external-docs.js';
+import componentsOrder from './rules/component-order.js';
 
-const infoDescription = require('./rules/info-description');
-const tagDescription = require('./rules/tag-description');
-const parameterDescription = require('./rules/parameter-description');
-const schemaDescription = require('./rules/schema-description');
-const schemaPattern = require('./rules/schema-pattern');
+import infoDescription from './rules/info-description.js';
+import tagDescription from './rules/tag-description.js';
+import parameterDescription from './rules/parameter-description.js';
+import schemaDescription from './rules/schema-description.js';
+import schemaPattern from './rules/schema-pattern.js';
 
-const checkPathParameter = require('./rules/path-parameter');
-const checkHeaderParameter = require('./rules/header-parameter');
-const checkQueryParameter = require('./rules/query-parameter');
-const checkTags = require('./rules/tags');
+import checkPathParameter from './rules/path-parameter.js';
+import checkHeaderParameter from './rules/header-parameter.js';
+import checkQueryParameter from './rules/query-parameter.js';
+import checkTags from './rules/tags.js';
 
-const ApiSchemaRequired = require('./decorators/api-schema-required');
-const StripXApiLevel = require('./decorators/strip-x-api-level');
+import ApiSchemaRequired from './decorators/api-schema-required.js';
+import StripXApiLevel from './decorators/strip-x-api-level.js';
 
-module.exports = () => ({
-  id: 'sfti',
-  rules: {
-    oas3: {
-      'data-types': checkSchemaName,
+export default function sftiPlugin() {
+  return {
+    id: 'sfti',
+    rules: {
+      oas3: {
+        'data-types': checkSchemaName,
 
-      'root-object-order' : checkObjectOrder,
-      'openapi-version': checkOpenapiVersion,
-      'info-content' : infoStructure,
-      'external-docs' : checkExternalDocs,
-      'component-order' : componentsOrder,
+        'root-object-order': checkObjectOrder,
+        'openapi-version': checkOpenapiVersion,
+        'info-content': infoStructure,
+        'external-docs': checkExternalDocs,
+        'component-order': componentsOrder,
 
-      'info-description' : infoDescription,
-      'tag-description' : tagDescription,
-      'parameter-description' : parameterDescription,
-      'schema-description' : schemaDescription,
-      'schema-pattern' : schemaPattern,
+        'info-description': infoDescription,
+        'tag-description': tagDescription,
+        'parameter-description': parameterDescription,
+        'schema-description': schemaDescription,
+        'schema-pattern': schemaPattern,
 
-      'path-parameter' : checkPathParameter,
-      'header-parameter' : checkHeaderParameter,
-      'query-parameter' : checkQueryParameter,
-      'tag' : checkTags,
-    }
-  },
-  decorators: {
-    oas3: {
-      'schema-required': ApiSchemaRequired,
-      'strip-x-api-level': StripXApiLevel,
+        'path-parameter': checkPathParameter,
+        'header-parameter': checkHeaderParameter,
+        'query-parameter': checkQueryParameter,
+        'tag': checkTags,
+      },
     },
-  },
-});
+    decorators: {
+      oas3: {
+        'schema-required': ApiSchemaRequired,
+        'strip-x-api-level': StripXApiLevel,
+      },
+    },
+  };
+}
